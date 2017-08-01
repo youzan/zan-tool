e44c5a0b2b2dea4eaaf17abca9590177a87ebe8a
kk <kk@youzan.com>
2017-08-01 16:29:45 +0800

    fix: zan template download path

--- src/config/template.js
+++ src/config/template.js
@@ -1,4 +1,4 @@
 module.exports = {
     NODE_MODULE: 'https://github.com/kk0829/node-module-template/archive/master.zip',
-    ZAN_NODE: 'https://github.com/youzan/zan-node/archive/master.zip'
+    ZAN_NODE: 'https://github.com/youzan/zan-template/archive/master.zip'
 };
