0d4709b882ba4fbad87a3b1af6320f1761751136
kk <kk@youzan.com>
2017-08-31 15:55:03 +0800

    feat: proxyUrl

--- src/plugins/dev/index.js
+++ src/plugins/dev/index.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "本地开发，开启后端服务",
     options: [
         ['--debug [debugName]', '开启 debug 模式'],
-        ['--mock', '开启 mock 模式，默认proxy地址为http://127.0.0.1:8001']
+        ['--mock [proxyUrl]', '开启 mock 模式，默认 proxy 地址为http://127.0.0.1:8001']
     ],
     action: action,
     help: () => {
@@ -16,6 +16,7 @@ module.exports = {
         console.log('    $ zan dev --debug koa:application');
         console.log('    $ zan dev --mock');
         console.log('    $ zan dev --mock https://127.0.0.1:8001');
+        console.log('    $ zan dev --debug --mock');
         console.log();
     }
 };
